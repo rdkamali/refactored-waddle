2252f517d3e1b0b5379dd10303ac7cd400b2d624
FaMaHo <108795546+FaMaHo@users.noreply.github.com>
2022-07-11 11:13:05 +0300

    Update Exercise_Three.py

--- Exercise_Three.py
+++ Exercise_Three.py
@@ -1,2 +1,14 @@
 # Exercise Three:
 # Write a python program average calculation for a class of ten students.
+a = input("Num1: ")
+b = input("Num2: ")
+c = input("Num3: ")
+d = input("Num4: ")
+e = input("Num5: ")
+f = input("Num6: ")
+g = input("Num7: ")
+h = input("Num8: ")
+i = input("Num9: ")
+j = input("Num10: ")
+s = int(a) + int(b) + int(c) + int(d) + int(e) + int(f) + int(g) + int(h) + int(i) + int(j)
+print(s/10)
