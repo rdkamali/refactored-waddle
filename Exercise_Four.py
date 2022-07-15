b7ac1e8d8fd1854526b2746a9412212308dea1f5
Kamali <rd.kamali@gmail.com>
2022-07-15 17:33:44 +0300

    Create Exercise_Four.py

---
+++ Exercise_Four.py
@@ -0,0 +1,15 @@
+# Test this sample, Remove # and see the results
+# -----------
+# print('My', 'Name', 'Is', 'James', sep='--')
+# -----------
+# num = 458.541315
+# print('%.2f' % num)
+# -----------
+# num = 8
+# print('%o' % num)
+# -----------
+# totalMoney = 1000
+# price = 450
+# statement1 = "I have {1} dollars so I can buy {0} football for {2:.2f} dollars."
+# print(statement1.format(quantity, totalMoney, price))
+# -----------
