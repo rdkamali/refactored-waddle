10ef03c17a9b346c1457015fe52f56ae9d2ab215
Kamali <rd.kamali@gmail.com>
2022-07-15 16:51:22 +0300

    Update Exercise_Three.py

--- Exercise_Three.py
+++ Exercise_Three.py
@@ -1,3 +1,8 @@
 # Exercise Three:
 # Write a python program average calculation for a class of ten students.
 # Hint: Use typecasting. Like float(input("Enter a number: "))
+# ---- 
+# num1 = int(input("Enter first number "))
+# num2 = int(input("Enter second number "))
+# res = num1 * num2
+# print("Multiplication is", res)
