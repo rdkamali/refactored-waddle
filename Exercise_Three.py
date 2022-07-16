570c6ee58a44fc7d3c5668651a1a68f9e6edbac0
MMRZA74 <mrajabi466@gmail.com>
2022-07-16 14:58:23 +0300

    Update Exercise_Three.py

--- Exercise_Three.py
+++ Exercise_Three.py
@@ -1,2 +1,5 @@
-# Exercise Three:
-# Write a python program average calculation for a class of ten students.
+list1 = [20,20,15,12,18,20,14,17,11,14]
+sum(list1)
+len(list1)
+avg = sum(list1)/len(list1)
+print("average of given number in list is:",avg)
