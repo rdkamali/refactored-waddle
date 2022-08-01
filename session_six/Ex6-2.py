9f5d562414851f4b094b7152c0506818ae4ac8fd
Kamali <rd.kamali@gmail.com>
2022-08-01 21:56:35 +0300

    Create Ex6-2.py

---
+++ session_six/Ex6-2.py
@@ -0,0 +1,27 @@
+#  3
+# 3 2 1
+# 2 1
+# 1
+# x = int(input("Enter a number: "))
+# while x>0:
+#     print(x)
+#     x=x-1
+# # print('End')
+# x = int(input("Enter a number: "))
+# while x>0:
+#     print(x, end=' ')
+#     x = x-1
+# print()    
+
+# ==========
+y = int(input("Enter a number: "))
+i = y
+while i>0:
+    
+    x = i
+    while x>0:
+        print(x, end=' ')
+        x = x-1
+    print()  
+    
+    i = i-1
