9225e3762e55829ddc5d7d054f7e48ede5e8696a
Kamali <rd.kamali@gmail.com>
2022-08-02 10:55:47 +0300

    Update Ex6-3.py

--- session_six/Ex6-3.py
+++ session_six/Ex6-3.py
@@ -1,12 +1,23 @@
+# 8 -> 4 2 1
+# x = 8
+# i -> 7 6 5 4 3 2 1
+listx =[]
 x = int(input("Enter a number: "))
-listx = []
-i=1
-while i < x:
+i = x - 1
+while i > 0:
     if x % i == 0:
         listx.append(i)
-    i = i + 1
-print("List ->",listx)
-sum = 0
-for item in listx:
-    sum = sum + item
-print('Sum of list ->',sum)
+    i = i - 1
+
+# if len(listx) == 1:
+#     print(x,'is a prime number')
+sum1 = sum(listx)
+# j = 0
+# while j < len(listx):
+#     sum = sum + listx[j]
+#     j = j + 1
+print('list->',listx)  
+  
+print('sum->',sum1)
+if sum1 == x:
+    print(x,'is a perfect number')
